02f35fb78d579d234dba44e6102c88f32b746c01
Valera <bomzadua@yomail.info>
2017-04-02 21:48:00 +0300

    save contact data on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,18 @@
 class ContactsController < ApplicationController
+
 	def new
 	
 	end
 	def create
+	@contact= Contact.new(contact_params)
 	render plain: params[:contact].inspect
+	@contact.save
 	end
+
+	private
+
+	def contact_params
+		params.require(:contact).permit(:email, :message)
+	end
+
 end
