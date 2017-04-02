4cdaca851302ac7a83450d444646d618e7038a03
Valera <bomzadua@yomail.info>
2017-04-02 21:06:41 +0300

    Contact model added

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321204509) do
+ActiveRecord::Schema.define(version: 20170402175842) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "contacts", force: true do |t|
+    t.string   "email"
+    t.string   "message"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
