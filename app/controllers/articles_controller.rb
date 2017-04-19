760c7c2206eba8c05e749261e0b89b4620e36953
Valera <bomzadua@yomail.info>
2017-04-19 16:24:27 +0300

    articles#create implemente

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,18 @@ def new
 end
 
 def create
-	render plain: params[:article].inspect
+	@article = Article.new(article_params)
+
+	if @article.valid?
+		@article.save
+	else
+		render action: 'new'
+	end
 end
 
+private
+
+	def article_params
+		params.require(:article).permit(:text, :title)
+	end
 end
